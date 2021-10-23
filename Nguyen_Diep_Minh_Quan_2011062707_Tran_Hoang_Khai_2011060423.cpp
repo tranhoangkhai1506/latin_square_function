0893cd5c9e2ad7038f6d7cc3898fe89e97ebc57f
KaaKa <91059573+tranhoangkhai1506@users.noreply.github.com>
2021-10-23 14:37:04 +0700

    first commit

---
+++ Nguyen_Diep_Minh_Quan_2011062707_Tran_Hoang_Khai_2011060423.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void latin_square(int arr[][100], int limit){				//			T(n)
+	for(int row = 0; row < limit; row++)					//				n
+      { 
+            for(int column = 0; column < limit; column++)			//			n
+            {	
+                  arr[row][column] = (row + column + limit) % limit + 1;	//		1
+		    }
+      }														//			T(n) = n * (n * 1)
+      														//				 = n^2 
+}
+int main()
+{
+      int limit;
+      int arr[100][100];
+      printf("Enter the limit:\t");
+      scanf("%d",&limit);
+      printf("\nLatin Square Matrix\n");
+      latin_square(arr, limit);
+      for(int row = 0; row < limit; row++)
+      {
+            printf("\n");
+            for( int column = 0; column < limit; column++)
+            {
+                  printf("%d ", arr[row][column]);
+            }
+      }
+      return 0;
+}
